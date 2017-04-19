4563a044c0b48f90f225ba98396a7d5dc5e3bd99
a <a>
2017-04-19 06:33:12 +0000

    post

--- microposts/app/controllers/application_controller.rb
+++ microposts/app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ class ApplicationController < ActionController::Base
 
   include SessionsHelper
 
-  private
+ private
 
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
   end
-end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
